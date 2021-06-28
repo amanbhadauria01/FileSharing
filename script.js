45148087a2a8e13f2ad3d79f896962d768a84874
amanbhadauria01 <amanbhadauria01@gmail.com>
2021-06-28 15:58:18 +0530

    file sharing

---
+++ script.js
@@ -0,0 +1,30 @@
+const File = require('./models/file');
+const fs   = require('fs');
+const connectDB = require('./config/db');
+connectDB();
+
+async function fetchData(){
+    // 24 hours old files are fetched and deleted
+    const pastDate = new Date(Date.now() - 24*60*60*1000);
+    const files = await File.find({ createdAt : { $lt : pastDate}});
+    if(files.length){
+        for(const file of files){
+            try{
+                // sync because we are deleting one by one 
+                // this is deleting from storage (i.e. upload folder) 
+                fs.unlinkSync(file.path);
+                // deleting from database
+                await file.remove();
+                console.log(`successfully deleted ${file.filename}`);                
+            }catch (err){
+                console.log(`Error while deleting file ${err}`);
+            }
+        }
+        console.log('Job done');
+    }
+}
+
+fetchData().then(()=>{
+    // to stop script
+    process.exit();
+});
