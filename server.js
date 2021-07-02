3e77ce037cc1b4195b78c60ca84bc69f6bca9182
amanbhadauria01 <amanbhadauria01@gmail.com>
2021-07-02 21:34:46 +0530

    cors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;    
+const PORT = 3000;    
 const connectDB = require('./config/db');
 const path = require('path');
 const cors = require('cors');
@@ -24,6 +24,12 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 // routes
+// app.use('/', function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "*");
+//     res.header("Access-Control-Allow-Methods", "POST" );
+//     next()
+// });
 app.use('/api/files',require('./routes/files'));
 app.use('/files',require('./routes/show'));
 app.use('/files/download',require('./routes/download'));
