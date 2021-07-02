c1d8396c95dbcdd5e196ca9818e8a0609956a0d8
amanbhadauria01 <amanbhadauria01@gmail.com>
2021-07-02 22:24:37 +0530

    cors

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const app = express();
 const PORT = 3000;    
 const connectDB = require('./config/db');
 const path = require('path');
+const cors = require("cors");
 require("dotenv").config();
 
 // connect database
 connectDB();
+
+app.use(cors());
+app.options('*', cors());
+// const corsOptions = {
+//     origin: process.env.ALLOWED_CLIENTS.split(","),
+// };
+  
+// app.use(cors(corsOptions));
 
 // static folder
 app.use(express.static('public'));
@@ -17,13 +26,13 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 // routes
-app.use('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Allow-Methods", "POST");
-    console.log("Passed 1");
-    next();
-});
+// app.use('/', function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "*");
+//     res.header("Access-Control-Allow-Methods", "POST");
+//     console.log("Passed 1");
+//     next();
+// });
 
 app.use('/api/files',require('./routes/files'));
 app.use('/files',require('./routes/show'));
