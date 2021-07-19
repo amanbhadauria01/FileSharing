e8319fb1fc82526f5743b81844ddd52c95bfefba
amanbhadauria01 <amanbhadauria01@gmail.com>
2021-07-19 10:04:25 +0530

    updates

--- server.js
+++ server.js
@@ -10,13 +10,9 @@ const CleanOldData = require('./services/checkmemory');
 // connect database
 connectDB();
 
+// cors
 app.use(cors());
 app.options('*', cors());
-// const corsOptions = {
-//     origin: process.env.ALLOWED_CLIENTS.split(","),
-// };
-  
-// app.use(cors(corsOptions));
 
 // static folder
 app.use(express.static('public'));
@@ -27,14 +23,6 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 // routes
-// app.use('/', function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "*");
-//     res.header("Access-Control-Allow-Methods", "POST");
-//     console.log("Passed 1");
-//     next();
-// });
-
 app.use('/api/files',require('./routes/files'));
 app.use('/files',require('./routes/show'));
 app.use('/files/download',require('./routes/download'));
